eb9681d80a825c626fdd37caa456f2fa57536c38
Reda Baydoun <reda.baydoun@gmail.com>
2025-07-24 16:15:41 -0400

    feat: WIP main

--- cmd/main.go
+++ cmd/main.go
@@ -42,8 +42,8 @@ func main() {
 	fmt.Println("\n\n--- Initializing capture on default device ---")
 	// Setup device to validate capture.
 	const (
-		sampleRate = 8000
-		blockSize  = 256
+		sampleRate = 44100
+		blockSize  = 2048
 		toneFreq   = 700.0
 	)
 
@@ -61,6 +61,7 @@ func main() {
 	// Avoid recreating these every time the onReceiveFrames function is called.
 	samples := make([]float64, blockSize)
 	freqSpectrum := make([]complex128, blockSize)
+	magnitudes := make([]float64, blockSize)
 	onReceiveFrames := func(_, iSamples []byte, sampleCount uint32) {
 		startTime := time.Now()
 
@@ -69,28 +70,100 @@ func main() {
 			samples[i] = fft.NormalizePCM16(int16(binary.LittleEndian.Uint16(iSamples[i<<1 : (i+1)<<1])))
 		}
 
-		// Window (limits spectral leakage)
-		windowing.Hann(samples)
+		// Window (reduces spectral leakage)
+		// Only apply it to the samples, not the padding.
+		windowing.Hann(samples[:sampleCount])
 
 		for i, s := range samples {
 			freqSpectrum[i] = complex(s, 0)
 		}
 
 		fft.IterativeFFT(freqSpectrum)
-		//var maxAmplitude int16 = 0
 
-		//for i := range sampleCount {
-		//	//for i := 0; i < int(sampleCount); i++ {
-		//	amplitude := int16(binary.LittleEndian.Uint16(iSamples[i<<1 : (i+1)<<1]))
-		//	if amplitude > maxAmplitude {
-		//		maxAmplitude = amplitude
-		//	}
-		//}
-
-		//fmt.Printf("Signal amplitude: %d             \r", maxAmplitude)
+		hannFactorRMS := windowing.HannFactorRMS(int(sampleCount))
+		halfSampleCount := int(sampleCount >> 1)
+		for i := range halfSampleCount {
+			// Normalize magnitudes and take into account the hanning window that was applied on the input samples before FFT.
+			magnitudes[i] = fft.ComputeMagnitude(freqSpectrum[i]) / hannFactorRMS
+		}
 
 		timeDiff := time.Now().Sub(startTime)
-		fmt.Printf("Processed %d in %d us\n", sampleCount, timeDiff/time.Microsecond)
+		fmt.Printf("Processed %d in %d us          \n", sampleCount, timeDiff/time.Microsecond)
+
+		for j := 9; j >= 0; j-- {
+//			for i := range halfSampleCount {
+//				meter := int(magnitudes[i] * 10)
+			if int(magnitudes[1] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[4] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[11] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[29] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[74] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[146] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[232] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[329] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[557] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+			if int(magnitudes[788] * 10) > j {
+				fmt.Print("::")
+			} else {
+				fmt.Print("  ")
+			}
+
+//				if meter >= j {
+//					fmt.Print("::")
+//				} else {
+//					fmt.Print("  ")
+//				}
+//			}
+			fmt.Println()
+		}
+
+		// Bring the cursor 10 lines up.
+		fmt.Print("\033[11A\r")
 	}
 
 	captureCallbacks := malgo.DeviceCallbacks{
