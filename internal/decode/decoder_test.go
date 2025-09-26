e4b8a751dd782d6a5680913d58666c22a1dc10db
Reda Baydoun <reda.baydoun@gmail.com>
2025-09-25 21:17:59 -0400

    feat: rework decoder tests

--- internal/decode/decoder_test.go
+++ internal/decode/decoder_test.go
@@ -1,49 +1,56 @@
 package decode
 
 import (
-	"fmt"
 	"testing"
 	"time"
 )
 
 func Test_Decode(t *testing.T) {
+	testCases := []struct {
+		name       string
+		detections []Detection
+		exp        string
+	}{
+		{
+			name: "single_character_E",
+			detections: []Detection{
+				{state: true, duration: 48 * time.Millisecond},
+				{state: false, duration: 3 * 48 * time.Millisecond},
+			},
+			exp: "E",
+		},
+	}
+
 	decodeIn := make(chan Detection)
 	decodeOut := make(chan string)
 	done := make(chan struct{})
-	expected := "ESA"
 
-	decoder := NewMorseDecoder(decodeIn, decodeOut, done, 25)
-	decoder.StartDecode()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			decoder := NewMorseDecoder(decodeIn, decodeOut, done, 25)
+			decoder.StartDecode()
 
-	output := ""
-	go func() {
-		for {
-			select {
-			case msg := <-decodeOut:
-				output = output + msg
-			case <-done:
-				fmt.Println("Done receiving")
-				return
+			output := ""
+			go func() {
+				for {
+					select {
+					case msg := <-decodeOut:
+						output = output + msg
+					case <-done:
+						return
+					}
+				}
+			}()
+
+			for _, detection := range tc.detections {
+				decodeIn <- detection
 			}
-		}
-	}()
+			time.Sleep(50 * time.Millisecond)
+			close(done)
 
-	decodeIn <- Detection{state: true, duration: 58 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 3 * 48 * time.Millisecond}
-	decodeIn <- Detection{state: true, duration: 37 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: true, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: true, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 3 * 48 * time.Millisecond}
-	decodeIn <- Detection{state: true, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 48 * time.Millisecond}
-	decodeIn <- Detection{state: true, duration: 3 * 48 * time.Millisecond}
-	decodeIn <- Detection{state: false, duration: 3 * 48 * time.Millisecond}
-	time.Sleep(50 * time.Millisecond)
-	close(done)
-
-	if output != expected {
-		t.Errorf("expecting %s, got %s", expected, output)
+			if output != tc.exp {
+				t.Errorf("expecting %s, got %s", tc.exp, output)
+			}
+		})
 	}
 }
